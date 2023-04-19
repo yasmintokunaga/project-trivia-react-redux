64ae64744ca647bdd42ee308dc2e6c741d376dfe
yasmintokunaga <yasmintkng@gmail.com>
2023-04-19 16:59:42 -0300

    feat(REQ01, REQ02): login page

---
+++ src/redux/index.js
@@ -0,0 +1,9 @@
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from '/path/to/your/root/reducer';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
+
+export default store;
